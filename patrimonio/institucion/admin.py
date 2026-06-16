965c657f335b366a5702a9b220eecb7e089e8250
Jeank935 <jccuenca17@utpl.edu.ec>
2026-06-16 12:12:46 -0500

    v2

--- patrimonio/institucion/admin.py
+++ patrimonio/institucion/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+# Al momento de presentar la información de museos, se debe presentar, 
+# además, el costo total de producción en función de los costos de las
+#  exhibiciones y el nombre (es) del guía con más experiencia
+from institucion.models import Museo,Guia_Museo,Exhibicion
+class ExhibicionAdmin(admin.ModelAdmin):
+    list_display = ('titulo_exhibicion', 'duracion_meses', 'costo_produccion','tematica')
+    
+    search_fields= ("titulo_exhibicion", 'tematica')
 
-# Register your models here.
+    def get_estudiante(self, obj):
+        """ """
+        return obj.estudiante.apellido
+admin.site.register(Exhibicion,ExhibicionAdmin)
+
+class MuseoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'ciudad', 'año_fundacion', 'get_exhibicion')
+    search_fields = ('nombre', 'ciudad')
+
+    def get_exhibicion(self, obj):
+        """ """
+        return obj.exhibicion.costo_produccion
+
+admin.site.register(Museo, MuseoAdmin)
+
--- patrimonio/institucion/models.py
+++ patrimonio/institucion/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-# Create your models here.
 
 class Museo(models.Model):
     nombre = models.CharField(max_length=30, unique =True)
@@ -10,14 +9,6 @@ class Museo(models.Model):
     def __str__(self):
         return f"Nombre {self.nombre} - Ciudad: {self.ciudad}- Año fundacion: {self.año_fundacion}"
 
-# Create your models here.
-
-# Guía de Museo:
-# Atributos:
-# nombre_completo: Nombres y apellidos completos del guía (cadena de texto).
-# años_experiencia_guia: Años de experiencia como guía de museo (número entero).
-# idiomas_hablados: Idiomas que el guía puede hablar (ej: "Español, Inglés") (cadena de texto).
-# Relación: un guía de museo trabaja en un museo)
 class Guia_Museo(models.Model):
     nombre_completo = models.CharField(max_length=60)
     años_experiencia_guia = models.IntegerField()
@@ -35,12 +26,14 @@ class Guia_Museo(models.Model):
 # tematica: Temática principal de la exhibición (cadena de texto).
 # Relación: una exhibición es asistida por un guía de museo
 class Exhibicion(models.Model):
-    nombre_completo = models.CharField(max_length=60)
-    años_experiencia_guia = models.IntegerField()
-    idiomas_hablados = models.CharField(max_length=100)
-    museo = models.ForeignKey(Museo, on_delete=models.CASCADE)
+    titulo_exhibicion = models.CharField(max_length=100)
+    duracion_meses = models.IntegerField()
+    costo_produccion = models.DecimalField(max_digits=10, decimal_places=2)
+    tematica = models.CharField(max_length=100)
+    guia = models.ForeignKey(Guia_Museo, on_delete=models.CASCADE, \
+                             related_name="guia_exhibicion")
 
     def __str__(self):
-        return f"Nombre Completo: {self.nombre_completo}- Anios Experiencia: {self.años_experiencia_guia}- Idiomas: {self.idiomas_hablados}"
+        return f"Titulo Exhibicion: {self.titulo_exhibicion}- Duracion Exhibicion: {self.duracion_meses}- Costo: {self.costo_produccion}- Tematica: {self.tematica}"
 
     
